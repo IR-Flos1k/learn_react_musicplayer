87eff4fae4fe53eb61e466cfb9b9e7c93877ac6a
Illia Cheliak <illia.che.dev@gmail.com>
2026-02-28 13:08:05 +0200

    chsnges in apiTSX

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -6,11 +6,18 @@ export type GetTrackDetailsOutputData = {
     }
 }
 
+const prepareHeaders = () => {
+    const apiKey = import.meta.env.VITE_API_KEY
+    if(!apiKey) return undefined
+
+    return{
+        'api-key': apiKey
+    }
+}
+
 export const getTrack = (trackId: string) => {
     const promise: Promise<GetTrackDetailsOutput> = fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + trackId, {
-      headers: {
-        'api-key': import.meta.env.VITE_API_KEY
-      }
+      headers: prepareHeaders()
     }).then(res => res.json())
 
     return promise
@@ -22,9 +29,7 @@ type GetTrackDetailsOutput ={
 
 export const getTracks = () => {
      const promise: Promise<GetTrackListOutput> = fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
-            headers: {
-                'api-key': import.meta.env.VITE_API_KEY
-            }
+            headers: prepareHeaders()
         }).then(res => res.json())
         return promise
 }
